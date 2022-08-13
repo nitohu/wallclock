d06bba95b79288bd01faeda0d3678fa0544f3937
nitohu <nitohu@gmail.com>
2022-08-14 00:27:11 +0200

    [ADD] logout

--- server/server/routers/home.js
+++ server/server/routers/home.js
@@ -2,6 +2,7 @@ const express = require("express")
 // const auth = require("../middleware/auth")
 const Settings = require("../models/settings")
 const Authentication = require("../models/authentication")
+const logger = require("../logger")
 
 const router = express.Router()
 
@@ -27,13 +28,14 @@ router.post("/", async (req, res) => {
         settings = await Settings.Authenticate(req.body.token)
     } catch (e) {
         // Login failed
-        console.warn(e)
+        logger.warn(e.message)
         return res.render("login", {
             settings,
             title: "Welcome",
             error: e.message
         })
     }
+    logger.info(`Login successful from ${ req.ip }`)
     // Authentication successful
     req.session.authenticated = true
     req.session.settings = settings
@@ -41,6 +43,15 @@ router.post("/", async (req, res) => {
         settings,
         title: "Welcome " + settings.username
     })
+})
+
+router.get("/logout", async (req, res) => {
+    if (!req.session) return res.redirect("/")
+
+    req.session.destroy((err) => {
+        if (err) logger.warn(`Error while destroying the session: ${err}`)
+    })
+    return res.redirect("/")
 })
 
 module.exports = router
