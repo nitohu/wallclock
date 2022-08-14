a79af92757e677a2fb4a34c99107bdb6bf665f7b
nitohu <nitohu@gmail.com>
2022-08-14 19:07:02 +0200

    [IMP] Extend device model, display list of devices

--- server/postgres/init/1-tables.sql
+++ server/postgres/init/1-tables.sql
@@ -12,18 +12,13 @@ CREATE TABLE device (
     id serial,
     primary key(id),
     name text,
-    ip text
-);
-
--- Merge w/ device or rename to e.g. device_settings
-CREATE TABLE auth (
-    id serial,
-    primary key(id),
+    ip text,
     active boolean,
-    name text,
     token text,
-    device_token boolean,
-    device_id int references device(id)
+    -- Last time a connection with device was established
+    last_conn timestamp,
+    create_date timestamp,
+    write_date timestamp
 );
 
 COMMIT;
--- server/server/models/device.js
+++ server/server/models/device.js
@@ -4,9 +4,15 @@ const logger = require("../logger")
 class Device {
     // Private fields
     #id = 0
+    #createDate = null
+    #writeDate = null
+    #lastConn = null
     // Public fields
     name = ""
     ip = ""
+    active = true
+    // TODO: Implement tokens properly, currently only placeholder
+    token = ""
 
     constructor(name, ip) {
         if (name !== undefined) this.name = name
@@ -15,6 +21,9 @@ class Device {
     
     // Public Methods
     getID() { return this.#id }
+    getCreateDate() { return this.#createDate }
+    getWriteDate() { return this.#writeDate }
+    getLastConn() { return this.#lastConn }
 
     // Create database record
     async create() {
@@ -23,14 +32,15 @@ class Device {
         if (String(this.name).trim() === "")
             throw new Error("Device has no name.")
 
-        let r = await db.query("INSERT INTO device(name, ip) VALUES ($1, $2) RETURNING *", [this.name, this.ip])
+        this.#createDate = new Date()
+        this.#writeDate = new Date()
+        const q = "INSERT INTO device(active, name, ip, token, create_date, write_date, last_conn) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id"
+        let r = await db.query(q, [this.active, this.name, this.ip, this.token, this.#createDate, this.#writeDate, this.#lastConn])
         if (!r || r.rowCount === 0)
             throw new Error("Something bad happened.")
         
         r = r.rows[0]
         this.#id = r.id
-        this.name = r.name
-        this.ip = r.ip
     }
     // Write to database
     async write() {
@@ -39,7 +49,9 @@ class Device {
         if (String(this.name).trim() === "")
             throw new Error("Device has no name.")
         
-        let r = await db.query("UPDATE device SET name=$2, ip=$3 WHERE id=$1 RETURNING id", [this.#id, this.name, this.ip])
+        this.#writeDate = new Date()
+        const q = "UPDATE device SET name=$2, ip=$3, active=$4, token=$5, write_date=$6, last_conn=$7 WHERE id=$1 RETURNING id"
+        let r = await db.query(q, [this.#id, this.name, this.ip, this.active, this.token, this.#writeDate, this.#lastConn])
         if (!r)
             throw new Error("Something bad happened.")
         if (r.rowCount === 0)
@@ -61,6 +73,11 @@ class Device {
         this.#id = r.id
         this.name = r.name
         this.ip = r.ip
+        this.active = r.active
+        this.token = r.token
+        this.#createDate = r.create_date
+        this.#writeDate = r.write_date
+        this.#lastConn = r.last_conn
     }
 
     // Gets all devices from database and returns them
@@ -72,6 +89,11 @@ class Device {
         for (let i = 0; i < r.rowCount; i++) {
             const d = new Device(r.rows[i].name, r.rows[i].ip)
             d.#id = r.rows[i].id
+            d.active = r.rows[i].active
+            d.token = r.rows[i].token
+            d.#createDate = r.rows[i].create_date
+            d.#writeDate = r.rows[i].write_date
+            d.#lastConn = r.rows[i].last_conn
             
             devices.push(d)
         }
