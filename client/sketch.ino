01d0c423d5f353fb29c50de30fd3aa497663130e
nitohu <nitohu@gmail.com>
2022-08-15 23:48:35 +0200

    [IMP] initial startup connection

--- client/sketch.ino
+++ client/sketch.ino
@@ -11,21 +11,14 @@ unsigned short r = 0, g = 0, b = 0;
 // Mode (will be necessary for some effects)
 int m = 0;
 // Brightness (0 -> 1)
-float brightness = 1.0f;
+float brightness = 0.3f;
 // Delay in ms (will be configurable)
 unsigned long del = 10;
 // Wifi stuff
 WiFiClient client;
 int wifiStatus = WL_IDLE_STATUS;
 
-void updateLeds() {
-    for (int i = 0; i < LED_COUNT; i++) {
-        int rb = r*brightness;
-        int gb = g*brightness;
-        int bb = b*brightness;
-        leds[i] = CRGB(rb, gb, bb);
-    }
-}
+void updateLeds();
 
 // Effects
 void fade_effect();
@@ -42,29 +35,38 @@ void setup() {
     // GRB -> activate rgb mode
     FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, LED_COUNT);
 
-    white();
-    updateLeds();
-    FastLED.show();
-
-    while(wifiStatus != WL_CONNECTED) {
+    // Try to connect to wifi
+    while (wifiStatus != WL_CONNECTED) {
         Serial.print("Try to connect to ");
         Serial.print(WIFI_SSID);
         Serial.println();
         // Try to connect
         wifiStatus = WiFi.begin(WIFI_SSID, WIFI_PASS);
-        if (wifiStatus == WL_CONNECT_FAILED) Serial.println("Connection failed for some reason.");
-        delay(5000);
+        if (wifiStatus == WL_CONNECTED) Serial.println("Connection success!");
+        else if (wifiStatus == WL_CONNECT_FAILED) { 
+            Serial.println("Connection failed for some reason.");
+            // Don't spam the router with requests
+            while (true) {}
+        } else delay(5000);
     }
-    Serial.println("4");
-    Serial.println("Connection success!");
 }
 
 void loop() {
+    /* Connection success */
     // Set colors
     fade_effect();
     // Update LED
     updateLeds();
     delay(del);
+}
+
+void updateLeds() {
+    for (int i = 0; i < LED_COUNT; i++) {
+        int rb = r*brightness;
+        int gb = g*brightness;
+        int bb = b*brightness;
+        leds[i] = CRGB(rb, gb, bb);
+    }
     FastLED.show();
 }
 
@@ -73,17 +75,27 @@ void fade_effect() {
     if (m == 0) {
         r++;
         if (b > 0) b--;
-        if (r >= 255) m++;
+        if (r >= 255) {
+            m++;
+            // Always make sure next number can be incremented
+            g = 0;
+        }
     }
     if (m == 1) {
         g++;
         if (r > 0) r--;
-        if (g >= 255) m++;
+        if (g >= 255) {
+            m++;
+            b = 0;
+        }
     }
     if (m == 2) {
         b++;
         if (g > 0) g--;
-        if (b >= 255) m = 0;
+        if (b >= 255) {
+            m = 0;
+            r = 0;
+        }
     }
 }
 
